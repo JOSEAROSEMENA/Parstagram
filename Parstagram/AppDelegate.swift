9b9b35a3bbdb9ba2e10490b3061e7e96a6b0bfce
JOSEAROSEMENA <77999340+JOSEAROSEMENA@users.noreply.github.com>
2021-08-10 21:15:50 -0400

    Parse + Sign up + Sign in
    Connected application to back4app backend Parse server
    
    Added sign up + sign in methods with Parse

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "CZX1zVj1p5m2JCKc7WJEA3oRd2sksVmPFTg7D8zy"
+                $0.clientKey = "PZN4FrZguHZxL25ilsYfNLkzO02w0rmVRWRtBzLP"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+
+        
         return true
     }
 
